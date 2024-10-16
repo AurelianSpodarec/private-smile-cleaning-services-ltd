63d257ee0476bca02abfcd418bd7ebbc08593d2e
aurelian.spod@gmail.com <aurelian.spod@gmail.com>
2024-10-16 14:33:26 +0100

    saving table - will do card because its better for mobile UI/UX

--- src/app/(web)/account/bookings/_components/BookingList.tsx
+++ src/app/(web)/account/bookings/_components/BookingList.tsx
@@ -4,6 +4,18 @@ import { useQuery } from "@tanstack/react-query";
 import { IBooking } from "@/interfaces/IBooking";
 import { cancelBooking, getCustomerBookingList } from "@/services/apis/launch27/requests/booking";
 
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+import { DataTable } from "./data-table";
+import { columns } from "./columns";
+
 function CardBookingExcerpt({ item }: { item: IBooking }) {
   const serviceDate = new Date(item.service_date);
 
@@ -21,42 +33,55 @@ function CardBookingExcerpt({ item }: { item: IBooking }) {
 
     console.log("cancel", a)
   }
+  console.log(item)
 
   return (
-    <div className="my-4 border">
-      <h3>Booking Details</h3>
-      <div>
-        <strong>Date:</strong> {serviceDate.toLocaleDateString()}
-      </div>
-      <div>
-        <strong>Address/Location:</strong> {item?.location?.name} {/* or full address */}
-      </div>
-      <div>
-        <strong>From:</strong> {startTime} <strong>To:</strong> {endTime}
-      </div>
-      <div>
-        <strong>Duration:</strong> {hours}h {minutes}m
-      </div>
-      <div>
-        <strong>Frequency:</strong> {item.frequency.name}
-      </div>
-      <div>
-        <strong>Service Type:</strong> {item.services[0].name}
-      </div>
-      <div>
-        <strong>Service Price:</strong> £{item.services[0].price.toFixed(2)}
-      </div>
-      <div>
-        <strong>Status:</strong> {item.completed ? 'Completed' : 'Pending'}
-      </div>
-      <div>
-        <strong>Total Price:</strong> £{item.summary.total.toFixed(2)}
-      </div>
-      <div>
-        <button>Edit</button>
-        <button onClick={() => handlerCancelBooking(item.id)}>Cancel</button>
-      </div>
-    </div>
+    //     <>
+    //       {/* <table className="min-w-full border-collapse border border-gray-200">
+    //     <thead>
+    //       <tr>
+    //         <th className="border border-gray-300 px-4 py-2">Address</th>
+    //         <th className="border border-gray-300 px-4 py-2">Status</th>
+    //         <th className="border border-gray-300 px-4 py-2">Date</th>
+    //         <th className="border border-gray-300 px-4 py-2">Time</th>
+    //         <th className="border border-gray-300 px-4 py-2">Frequency</th>
+    //         <th className="border border-gray-300 px-4 py-2">Service Type</th>
+    //         <th className="border border-gray-300 px-4 py-2">Total Price</th>
+    //         <th className="border border-gray-300 px-4 py-2">Total Refunded</th> {/* New Column */}
+    //       <th className="border border-gray-300 px-4 py-2">Actions</th>
+    //     </tr >
+    //     </thead >
+    //     <tbody>
+    //       <tr>
+    //         <td className="border border-gray-300 px-4 py-2">{item?.address?.full_address}</td>
+    //         <td className="border border-gray-300 px-4 py-2">{item.completed ? 'Completed' : 'Pending'}</td>
+    //         <td className="border border-gray-300 px-4 py-2">{serviceDate.toLocaleDateString()}</td>
+    //         <td className="border border-gray-300 px-4 py-2">{startTime} - {hours}h {minutes}m - {endTime}</td>
+    //         <td className="border border-gray-300 px-4 py-2">{item.frequency.name}</td>
+    //         <td className="border border-gray-300 px-4 py-2">{item.services[0].name}</td>
+    //         <td className="border border-gray-300 px-4 py-2">£{item.summary.total.toFixed(2)}</td>
+    //         {/* <td className="border border-gray-300 px-4 py-2">£{item.summary.refunded.toFixed(2)}</td> Display Total Refunded */}
+    //         <td className="border border-gray-300 px-4 py-2">
+    //           {/* <button onClick={() => handlerEdit(item.id)}>Edit</button> */}
+    //           {/* <button onClick={() => handlerRebook(item.id)}>Rebook</button> */}
+    //           <button onClick={() => handlerCancelBooking(item.id)}>Cancel</button>
+    //         </td>
+    //       </tr>
+    //       {/* Additional rows can go here */}
+    //     </tbody>
+    //   </table >
+
+    //     <div className="p-4">
+    //       hi
+    //     </div> */
+    // }
+    //   </>
+    <TableRow>
+      <TableCell className="font-medium">INV001</TableCell>
+      <TableCell>Paid</TableCell>
+      <TableCell>Credit Card</TableCell>
+      <TableCell className="text-right">$250.00</TableCell>
+    </TableRow>
   );
 }
 
@@ -67,17 +92,30 @@ function BookingList() {
     queryKey: ['bookingList'],
     queryFn: async () => await getCustomerBookingList()
   })
-  const data = bookingList.data
+  // const data = bookingList.data
+  const data =[{id: 1}]
 
 
   console.log(data)
 
   return (
-    <div>
-      {data?.map((item) => {
-        return <CardBookingExcerpt item={item} key={item.id} />
-      })}
-    </div>
+    // <Table>
+    //   <TableCaption>A list of your recent invoices.</TableCaption>
+    //   <TableHeader>
+    //     <TableRow>
+    //       <TableHead className="w-[100px]">Invoice</TableHead>
+    //       <TableHead>Status</TableHead>
+    //       <TableHead>Method</TableHead>
+    //       <TableHead className="text-right">Amount</TableHead>
+    //     </TableRow>
+    //   </TableHeader>
+    //   <TableBody>
+    //     {data?.map((item) => {
+    //       return <CardBookingExcerpt item={item} key={item.id} />
+    //     })}
+    //   </TableBody>
+    // </Table>
+    <DataTable data={data} columns={columns} />
   );
 }
 
