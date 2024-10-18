058e02e869e53a23172537ff66ce21141c6b4c53
rashad-smile <rashad@smile.cleaning>
2024-10-18 21:26:55 +0000

    another update

--- next.config.mjs
+++ next.config.mjs
@@ -4,9 +4,11 @@ import dotenv from 'dotenv';
 // Load environment variables from the .env file
 dotenv.config({ path: '/var/www/smile-cleaning/staging/shared/.env' });
 
-// Your existing Next.js config
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   async redirects() {
     return [
       {
